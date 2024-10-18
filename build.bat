7c20750fb4e182c9d73fbf3b3af2097b92f51040
NoxNode <noxnode@gmail.com>
2024-10-17 23:32:29 -0700

    removed powershell script and put the powershell code in the batch script

--- build.bat
+++ build.bat
@@ -1,1 +1,2 @@
-powershell.exe -executionpolicy remotesigned -File  build.ps1
+powershell -Command "& { $hex_string = Get-Content -Path 'hrmc.dmp' ; $hex_string = $hex_string -replace '#.*', '' ; $hex_string = $hex_string -join '' ; $hex_string = $hex_string -replace '\s+', ''  ; $hex_string_spaced = $hex_string -replace '..', '0x$& ' ; $byte_array = [byte[]] -split $hex_string_spaced ; Set-Content -Path 'hrmc.exe' -Value $byte_array -Encoding Byte } "
+
